a7e717ec8972ca946bb92adb4e65ada829cd94e0
Milen Halachev <milenski88@gmail.com>
2016-05-27 16:11:58 +0300

    Update install_transmission.sh
    added sudo for transmission start and stop

--- transmission/install_transmission.sh
+++ transmission/install_transmission.sh
@@ -6,7 +6,7 @@ sudo apt-get install transmission-daemon
 #add the debian-transmission user to the osmc group
 sudo usermod -a -G osmc debian-transmission
 
-/etc/init.d/transmission-daemon stop
+sudo /etc/init.d/transmission-daemon stop
 cp transmission_settings.json /etc/transmission-daemon/settings.json
 cp torrent-done.sh /home/osmc/debian-transmission/torrent-done.sh
-/etc/init.d/transmission-daemon start
+sudo /etc/init.d/transmission-daemon start
