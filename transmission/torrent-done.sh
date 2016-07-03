9498c20c2593910919b080800cda1645d79e6122
Milen Halachev <milenski88@gmail.com>
2016-07-03 22:59:09 +0300

    Update torrent-done.sh
    - set CouchPotato API Key as variable

--- transmission/torrent-done.sh
+++ transmission/torrent-done.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+CP_API_KEY=22bd1c8b35a7429985547f5a349f7b3d
+
 #trigger sickrage post processing for TVShows dir
 python /home/osmc/Install/sickrage/autoProcessTV/sabToSickBeard.py /home/osmc/Downloads/TVShows/
 
 #trigger couchpotato post processing for Movies dir
-wget -qO /dev/null http://localhost:5050/api/4ffad3e8fc7646c09b63420a0ed53550/renamer.scan
+wget -qO /dev/null http://localhost:5050/api/$CP_API_KEY/renamer.scan
 
 #move MoviesBG
 mv /home/osmc/Downloads/MoviesBG/* /home/osmc/MoviesBG/
