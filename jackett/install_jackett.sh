3ec46be6deae414f8c3c28b9ce15fbae181ba7c6
Milen Halachev <milenski88@gmail.com>
2017-10-15 15:01:34 +0300

    added jackett service installation

--- jackett/install_jackett.sh
+++ jackett/install_jackett.sh
@@ -6,5 +6,10 @@ wget $(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | jq
 tar -zxvf Jackett.Binaries.Mono.tar.gz
 mv Jackett /home/osmc/Install/jackett
 
+#setup the service
+sudo cp init.osmc /etc/init.d/jackett
+sudo chmod +x /etc/init.d/jackett
+sudo update-rc.d jackett defaults
+
 #cleanup
 rm Jackett.Binaries.Mono.tar.gz
