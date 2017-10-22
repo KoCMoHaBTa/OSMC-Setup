0de19915bbefef461704b51169aa5382a4be086a
Milen Halachev <milenski88@gmail.com>
2017-10-22 18:25:37 +0300

    disabled dependencies required for jackett installation

--- basic/install_dependencies.sh
+++ basic/install_dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #add mono repository (raspbian 8 - jessie - works for osmc) - required for jackett
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian raspbianjessie main" | sudo tee /etc/apt/sources.list.d/mono-official.list
-sudo apt-get update
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+#echo "deb http://download.mono-project.com/repo/debian raspbianjessie main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+#sudo apt-get update
 
 #install mono - required for jackett
-sudo apt-get install mono-devel -y
+#sudo apt-get install mono-devel -y
 
 #install p7zip for extraction zip files
 sudo apt-get install p7zip-full -y
