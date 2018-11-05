5afdb6847fd9329731f5d29522320fb4b5c44462
Milen Halachev <milenski88@gmail.com>
2018-11-06 00:06:51 +0200

    Update torrent-done.sh
    - updated sickrage post processing trigger

--- transmission/torrent-done.sh
+++ transmission/torrent-done.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+SR_API_KEY=dda2872252d9c377d43e8ffd451f9b59
 CP_API_KEY=22bd1c8b35a7429985547f5a349f7b3d
 
 #trigger sickrage post processing for TVShows dir
-python /home/osmc/Install/sickrage/autoProcessTV/sabToSickBeard.py /home/osmc/Downloads/TVShows/
+wget -qO /dev/null http://localhost:8081/api/$SR_API_KEY/?cmd=postprocess
 
 #trigger couchpotato post processing for Movies dir
 wget -qO /dev/null http://localhost:5050/api/$CP_API_KEY/renamer.scan
